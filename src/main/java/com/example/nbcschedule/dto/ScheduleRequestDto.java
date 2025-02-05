12037bd2d15d7700733529363b6969a5d8054309
polaris65b <polaris65b@gmail.com>
2025-02-05 14:28:47 +0900

    RequsetDto

---
+++ src/main/java/com/example/nbcschedule/dto/ScheduleRequestDto.java
@@ -0,0 +1,11 @@
+package com.example.nbcschedule.dto;
+
+import lombok.Getter;
+
+@Getter
+public class ScheduleRequestDto {
+
+    private String task;
+    private String name;
+    private String password;
+}
