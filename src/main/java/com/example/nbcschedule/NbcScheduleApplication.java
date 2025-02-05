a616836144efba9cdf440c4481d24ef66e614188
polaris65b <polaris65b@gmail.com>
2025-02-05 13:29:11 +0900

    test

---
+++ src/main/java/com/example/nbcschedule/NbcScheduleApplication.java
@@ -0,0 +1,13 @@
+package com.example.nbcschedule;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NbcScheduleApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(NbcScheduleApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/nbcschedule/NbcScheduleApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.nbcschedule;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NbcScheduleApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
