685ae757972bdd40dff7acb75ac03e6ad69862cb
polaris65b <polaris65b@gmail.com>
2025-02-05 15:28:37 +0900

    전체 일정 조회 Controller

--- src/main/java/com/example/nbcschedule/controller/ScheduleController.java
+++ src/main/java/com/example/nbcschedule/controller/ScheduleController.java
@@ -5,9 +5,9 @@ import com.example.nbcschedule.dto.ScheduleResponseDto;
 import com.example.nbcschedule.service.ScheduleService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RestController
 @RequiredArgsConstructor
@@ -15,10 +15,17 @@ public class ScheduleController {
 
     private final ScheduleService scheduleService;
 
+    // 일정 생성
     @PostMapping("/schedules")
     public ResponseEntity<ScheduleResponseDto> save(
             @RequestBody ScheduleRequestDto dto
     ) {
         return ResponseEntity.ok(scheduleService.save(dto));
+    }
+
+    // 전체 일정 조회
+    @GetMapping("/schedules")
+    public ResponseEntity<List<ScheduleResponseDto>> findAll() {
+        return ResponseEntity.ok(scheduleService.findAll());
     }
 }
